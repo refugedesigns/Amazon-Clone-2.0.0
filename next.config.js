0d2d9cc0d943b5b6be31c567d1571d2c55a1901a
Erasmus Antwi <refugedesigns17@gmail.com>
2021-07-27 18:06:42 +0000

    debugging stripe

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
   env: {
     MONGO_USER: process.env.MONGO_USER,
     MONGO_PASSWORD: process.env.MONGO_PASSWORD,
-    STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY,
+    STRIPE_PUBLIC_KEY: `${process.env.STRIPE_PUBLIC_KEY}`,
     STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
     HOST: process.env.HOST,
     STRIPE_SIGNING_SECRET: process.env.STRIPE_SIGNING_SECRET,
