3a1416e72589153b09bd78e8068ac881307ba05a
Erasmus Antwi <refugedesigns17@gmail.com>
2021-07-29 09:28:57 +0000

    lock success page

--- src/pages/success.js
+++ src/pages/success.js
@@ -42,6 +42,15 @@ export default SuccessPage;
 export async function getServerSideProps(context) {
   const session = await getSession({ req: context.req });
 
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+  
   return {
     props: {
       session,
